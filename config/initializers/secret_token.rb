9b608ae027fc8a5f5fca5626d3e0f545c8deaed4
gin <jineok12@gmail.com>
2012-09-07 10:12:09 +0900

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kakaoreview::Application.config.secret_token = '9d7831052185b3ef1ca95a02a990a6309046438476117e2b560ba037b84d425486136cd0136c185e028f85dedc80902dbaf553a273626ea3794e43e1b70a1d74'
